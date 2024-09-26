007d25188a59c9ba735f16f7206fb27a76ea1f66
Micah <mphelps@twilio.com>
2024-09-26 00:26:30 -0700

    updated contact

--- src/components/pages/Bio.js
+++ src/components/pages/Bio.js
@@ -60,29 +60,29 @@ const Bio = () => {
                             Biography
 </div>
 
-<p className ='absolute items-center justify-center ml-32 mr-10 mt-20 bg-white text-lg object-contain rounded-md'>
-<div style={containerStyle}>
-      <p style={paragraphStyle}>
+<p className ='max-w-3xl absolute items-center text-sm justify-center ml-32 mr-32 mt-20 bg-white text-lg object-contain rounded-md'>
+<div>
+      <p>
         Jason is an artist and art educator in the American Pacific Northwest. He was raised around the 
          <em> mountains and rivers</em> of Southern Oregon, where he fostered an instinct to wander, collect, and create. Jason worked as a vineyard hand, forester, and carpenter, all of which contributed to shaping his aesthetics, content, and process. He went on to receive his <span style={highlightedStyle}>Bachelor of Fine Arts</span> degree in both painting and sculpture at Southern Oregon University. Being heavily influenced by <span style={italicStyle}>Joseph Beuys</span> and the notion that art can have a transformative effect on the people who experience it, Jason decided to pursue his <span style={highlightedStyle}>Master's in the Arts of Teaching</span> from Pacific University in Oregon.
       </p>
       <br />
-      <p style={paragraphStyle}>
+      <p>
         Being among the mountains and rivers is still instrumental to his process. Dirt, roots, shells, plants, and rust are often combined in paint, plaster, wax, or resin. In the spring of 2010, Jason exhibited "<span style={italicStyle}>Under the Wing of Corvus</span>" at the <span style={highlightedStyle}>Angst Gallery</span> in Vancouver, Washington. The exhibition explored the art, literature, and mythology of <span style={italicStyle}>crows and ravens</span>. He has since followed up with <span style={italicStyle}>Mythos</span> and <span style={italicStyle}>Sojourn</span>.
       </p>
       <br />
-      <p style={paragraphStyle}>
-        Currently, Jason has turned his process to exploring the connections between <span style={wildernessStyle}>Wilderness</span> and <span style={wildernessStyle}>Psyche</span> in the age of the Anthropocene. He wishes to examine the importance of Wilderness for our mind and soul in an age when so much of the planet has been touched by human endeavor - as in the way we look to the night sky and see more <em>satellites than shooting stars</em>. He aims to capture the paradox of a simulated Wild on the planet. Furthermore, Jason wants to imagine how unlocking the secrets of nature can be <span style={highlightedStyle}>mutually beneficial</span> for humans and nature, i.e., green energy. Landscape and Wilderness are explored through wandering, mapping, and collecting artifacts of both the mind and the physical landscape. Forms created range from wall relief to freestanding sculpture. Jason hopes the viewer will be initially impressed by <span style={italicStyle}>diverse forms</span> and matter, then consider the implications and poetics of works inspired by maps, charts, and landscape. Lastly, Jason wants to question whether true freedom of the mind and body requires both a real and perceived Wilderness.
+      <p>
+        Currently, Jason has turned his process to exploring the connections between Wilderness and Psyche in the age of the Anthropocene. He wishes to examine the importance of Wilderness for our mind and soul in an age when so much of the planet has been touched by human endeavor - as in the way we look to the night sky and see more <em>satellites than shooting stars</em>. He aims to capture the paradox of a simulated Wild on the planet. Furthermore, Jason wants to imagine how unlocking the secrets of nature can be <span style={highlightedStyle}>mutually beneficial</span> for humans and nature, i.e., green energy. Landscape and Wilderness are explored through wandering, mapping, and collecting artifacts of both the mind and the physical landscape. Forms created range from wall relief to freestanding sculpture. Jason hopes the viewer will be initially impressed by <span style={italicStyle}>diverse forms</span> and matter, then consider the implications and poetics of works inspired by maps, charts, and landscape. Lastly, Jason wants to question whether true freedom of the mind and body requires both a real and perceived Wilderness.
       </p>
-      <blockquote style={quoteStyle}>
+      <p> <br/>
         "What personal and global liberties are we willing to negotiate as we move deeper into the Anthropocene? Only when these questions are considered can we act to provide the planet with the rights we expect for ourselves; and upon doing so, we may be able to find balance in our interactions with the place we all call home."
-      </blockquote>
+        </p>
       <br />
-      <p style={paragraphStyle}>
-        As an artist and an educator, Jason believes strongly in the <span style={highlightedStyle}>transformative effect</span> art can have upon a community.
+      <p>
+        As an artist and an educator, Jason believes strongly in the transformative effect art can have upon a community.
       </p>
       <br />
-      <p style={paragraphStyle}>
+      <p>
         Contact Jason for commissions or collaboration at 
         <a href="mailto:jasonwadephelps@gmail.com" style={emailStyle}> jasonwadephelps@gmail.com</a>
       </p>
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -35,53 +35,48 @@ const instragramClick = () =>{
 <div className='ml-32 relative z-30 font-redhat text-3xl text-black'>
                             Contact
 </div>
-<div className ='relative mt-20 min-w-min pb-10 inset-0 flex items-center justify-center ml-32 mr-10 bg-stone-200 text-lg object-contain rounded-md'>
 
-<div className='min-w-min pb-10 pt-10 mr-10 ml-10 mt-10 bg-cyan-700 rounded-md text-center justify-center items-center flex'>
-
-  <div className='text-slate-300 ml-10 mb-10'>
-    <p>
-      <strong>Purchasing inquiries, proposals for representation, and general questions</strong> should be sent via email to:
-    </p>
-    <br />
-    <p className='font-bold hover:text-white'>
-      <a href="mailto:jasonwadephelps@gmail.com">jasonwadephelps@gmail.com</a>
-    </p>
-    <br />
-    <p>
-      See more of my progress and inspiration on social media at:
-    </p>
-  </div>
-  <div className='mr-10 ml-10'>
-
-<div onClick = {instragramClick} className='hover:opacity-50 cursor-pointer mt-10'>
-  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-instagram" viewBox="0 0 16 16">
-    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
-  </svg>
-</div>
-
-<div onClick = {facebookClick} className='mt-10 hover:opacity-50 cursor-pointer'>
-<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-<path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
-</svg>
-</div>
-</div>
-
-  <div className='h-96 mt-10 flex'>
-
-    <div className = 'mr-10'>
+<div className='absolute ml-32 text-black mt-20 mr-10 mb-10 grid grid-cols-2 col-start-2 border-black border-2'>
+<div className = 'mr-10'>
     <motion.img
       src={jason}
       className="hover:opacity-50 z-25 object-contain w-full h-full"
     />
 </div>
 
+<div className='text-black mb-10 flex items-center justify-center flex-col'>
+<div>
+    <p>
+     
+Purchasing inquiries, proposals for representation, <br/>and general questions should be sent via email to: 
+    </p>
+    <br />
+    <p className='font-bold hover:text-slate-300'>
+      <a href="mailto:jasonwadephelps@gmail.com">jasonwadephelps@gmail.com</a>
+    </p>
+    <br />
+    <p>
+      See more of my progress and inspiration on social media at:
+    </p>
+    </div>
 
+<div className='flex items-center justify-center flex-row'>
 
+<div onClick = {instragramClick} className='hover:opacity-50 cursor-pointer mt-10'>
+  <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" className="bi bi-instagram" viewBox="0 0 16 16">
+    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+  </svg>
+</div>
+
+<div onClick = {facebookClick} className='mt-10 ml-10 hover:opacity-50 cursor-pointer'>
+<svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+<path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+</svg>
+</div>
+</div>
   </div>
-</div>
+  </div>
 
-</div>
 </div>
 
 
